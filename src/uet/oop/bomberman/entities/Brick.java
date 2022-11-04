7a0c68694a0f4fbd5b6074dcbb6400292687e2c8
Phí Minh Hiếu <98246591+Angelectronic@users.noreply.github.com>
2022-11-04 16:00:02 +0700

    fix bomb

--- src/uet/oop/bomberman/entities/Brick.java
+++ src/uet/oop/bomberman/entities/Brick.java
@@ -6,6 +6,8 @@ import uet.oop.bomberman.BombermanGame;
 import uet.oop.bomberman.graphics.Sprite;
 
 public class Brick extends Entity {
+   private int timeToDie = 50;
+
     public Brick(int x, int y, Image img) {
         super(x, y, img);
         this.collisionBox = new Rectangle(x * Sprite.SCALED_SIZE, y * Sprite.SCALED_SIZE, img.getWidth(), img.getHeight());
@@ -14,11 +16,13 @@ public class Brick extends Entity {
     @Override
     public void update() {
         if (!isAlive) {
-            if (timeToDie > -10) {
+            if (timeToDie > 0) {
                 this.setImg(Sprite.movingSprite(Sprite.brick_exploded2,
                         Sprite.brick_exploded1, Sprite.brick_exploded, timeToDie, 60).getFxImage());
                 timeToDie--;
                 BombermanGame.map[yUnit][xUnit] = "+";
+            } else {
+                this.setImg(null);
             }
         }
     }
